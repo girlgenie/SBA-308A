55aaaad85008544d3c3ffd994dca1d8ebde8d08f
Rockelle Gardner <rockellegardner@gmail.com>
2024-03-09 08:27:45 -0500

    youtube image

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const button = document.getElementById('search-button');
 button.addEventListener("click", ()=> { 
   // user input
   console.log(userInput.value)
+  userInput.value = ''; 
 
   // 
 
@@ -10,15 +11,7 @@ button.addEventListener("click", ()=> {
   console.log(button)
 })
 
-// delete code for button below if button above works
-// const button2 = document.createElement("button"); 
-// button2.textContent= "Title"
-// document.body.appendChild(button2);
-//  button2.addEventListener("click", (onclick) => {
-//   console.log("button clicked")
-// }).
-// delete code for button above if button above works 
-
+//  YOUTUBE VIDEO HELP: https://www.youtube.com/watch?v=QegE9i4UW4I&t=401s&ab_channel=WinDev 
 // const url =
 //   "https://youtube-search-results.p.rapidapi.com/youtube-search/?q=justin%2Bbieber";
 //   const options = {
