d7bfead83cacbd8d2035e30e3e347da19af2350c
Rockelle Gardner <rockellegardner@gmail.com>
2024-03-09 06:58:07 -0500

    add search feature

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const button = document.createElement("button"); 
+button.textContent= "Title"
+document.body.appendChild(button);
+ button.addEventListener("click", (onclick) => {
+  console.log("button clicked")
+})
+
 const url =
   "https://youtube-search-results.p.rapidapi.com/youtube-search/?q=justin%2Bbieber";
-const options = {
+  const options = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "b55e621a8bmshb379002c4bd8badp17c68djsn1f53bb5cc1a1",
@@ -10,7 +17,7 @@ const options = {
 async function fetchData (){
   try {
     const response = await fetch(url, options);
-    const result = await response.json();
+    const result = await response.json(response);
     console.log(result);
   } catch (error) {
     console.error(error);
